bbbafd099f1954f6c711dc6f9f2b078db7fe9055
Dusty Zietz <dustyzietz@gmail.com>
2019-09-21 14:51:33 -1000

    Updates

---
+++ precache-manifest.69e9b12895d41c578e312901521fef14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe25ecd6a95d5203eab945e9f0d68af4",
+    "url": "/sgr1.1/index.html"
+  },
+  {
+    "revision": "b27b4906ee0d70774528",
+    "url": "/sgr1.1/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f269d0cf62e9eb870dcd",
+    "url": "/sgr1.1/static/js/2.1298558f.chunk.js"
+  },
+  {
+    "revision": "b27b4906ee0d70774528",
+    "url": "/sgr1.1/static/js/main.abe6be26.chunk.js"
+  },
+  {
+    "revision": "6e1c9e0f8b116dded013",
+    "url": "/sgr1.1/static/js/runtime~main.ab6afb98.js"
+  }
+]);
